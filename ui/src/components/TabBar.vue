5b899452d604107742f7f789e54b8f467fd8b1eb
zhengyi <admin@mczhengyi.top>
2024-01-13 19:39:10 +0800

    :sparkles: Add selected states for tabbar

--- ui/src/components/TabBar.vue
+++ ui/src/components/TabBar.vue
@@ -4,29 +4,26 @@ import Card from "./Card.vue";
 import TabItem from "./TabItem.vue";
 import {ref} from "vue";
 
+const files = ref<{[key: string]: string}>({
+  "/": "Home",
+  "/resources": "Resources"
+})
+const selectedFile = ref("/")
 
-const files = ref(["Resource", "Download"])
-
-function closeHandler(payload: object) {
-  console.log(files.value.indexOf(payload.name))
-  console.log(files.value.toString())
-  const index = files.value.indexOf(payload.name)
-
-  files.value = files.value.splice(index + (index >= files.value.length ? 0 : 1), 1)
-  console.log(files.value.toString())
+function closeHandler(payload: {path: string}) {
+  delete files.value[payload.path]
 }
-
 </script>
 
 <template>
   <card class="tab-bar">
     <div class="tab-item-group">
-      <tab-item v-for="file in files" :name="file" @close="closeHandler"/>
+      <tab-item v-for="(file, path) in files" :name="file" :path="path" :open="selectedFile===path" @close="closeHandler" @click="selectedFile=path"/>
     </div>
   </card>
 </template>
 
-<style scoped>
+<style scoped lang="postcss">
 .tab-bar {
   @apply mx-6 my-4 h-12 p-1.5;
 }
