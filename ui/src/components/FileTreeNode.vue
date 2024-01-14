2048a71a5f2e76f5af02cb276cf97b505c5eeded
Colter23 <colter_null@163.com>
2024-01-14 18:40:26 +0800

    :sparkles: update: file tree

--- ui/src/components/FileTreeNode.vue
+++ ui/src/components/FileTreeNode.vue
@@ -1,28 +1,37 @@
 <script setup lang="ts">
+import Icon from "./Icon.vue";
+
 const props = defineProps({
   deep: {
     type: Number,
     default: 0
-  }
+  },
+  content: String
 });
-
-// const retract = 0
 
 </script>
 
 <template>
 <div class="tree-node">
-  <div class="tree-indent" v-for="_ in deep"></div>
-  <div class="inline-flex"></div>
-  <div>AAA</div>
+  <div class="node-indent" v-for="_ in deep"></div>
+  <div class="node-fold-icon">
+    <icon icon="icon-unfold" size="normal"></icon>
+  </div>
+  <div class="node-content">{{ content }}</div>
 </div>
 </template>
 
-<style scoped>
+<style scoped lang="postcss">
 .tree-node {
-  @apply flex w-full h-7 rounded-md bg-amber-500
+  @apply flex w-full h-7 px-1 rounded-md hover:bg-[#EBF3FF]
 }
-.tree-indent {
-  @apply w-6 grow-0 shrink-0
+.node-indent {
+  @apply w-7 grow-0 shrink-0
+}
+.node-fold-icon {
+  @apply w-7 inline-flex grow-0 shrink-0 items-center justify-center cursor-pointer transition-transform -rotate-90
+}
+.node-content {
+  @apply inline-flex grow items-center cursor-pointer
 }
 </style>
