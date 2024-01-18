84a2676f71ad262d95475f0ac43dc7dd07f7ee30
Colter23 <colter_null@163.com>
2024-01-18 17:16:12 +0800

    :construction: use api

--- ui/src/views/MainView.vue
+++ ui/src/views/MainView.vue
@@ -4,36 +4,31 @@ import TabBar from "../components/TabBar.vue";
 import FileTree from "../components/FileTree.vue";
 import {ref} from "vue";
 import {FileTreeData} from "../class";
+import {useFileStore} from "../store";
+import {getFolderData} from "../network/api";
+
+const fileStore = useFileStore();
 
 
-const data = ref<FileTreeData[]>([
-  {
-    path: "/Res",
-    name: "Res",
-  }
-])
+const treeData = ref<FileTreeData[]>([]);
+
 const handleLoad = (node: FileTreeData) => {
   return new Promise<void>(async (resolve) => {
-    console.log("LoadData")
-    node.children = [
-      {
-        path: "/Res1",
-        name: "Res1",
-      },
-      {
-        path: "/Res2",
-        name: "Res2",
-      }
-    ];
+    console.log("[Load] " + node.path);
+    node.children = fileStore.saveAndConvertFolderData(await getFolderData(node.path));
     resolve();
   });
 }
+
+getFolderData().then(data => {
+  treeData.value = fileStore.saveAndConvertFolderData(data);
+})
 
 </script>
 
 <template>
   <tab-bar @change="(e) => {console.log(e)}"></tab-bar>
-  <file-tree :data="data" :load-data="handleLoad"></file-tree>
+  <file-tree :data="treeData" :load-data="handleLoad"></file-tree>
 </template>
 
 <style scoped lang="postcss">
