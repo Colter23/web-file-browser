112f7f88859d970c04a6d6300c931d3dd35bab11
Colter23 <colter_null@163.com>
2024-02-16 22:41:36 +0800

    :construction: update breadcrumb

--- ui/src/components/Breadcrumb.vue
+++ ui/src/components/Breadcrumb.vue
@@ -2,57 +2,79 @@
 
 import SubCard from "./SubCard.vue";
 import Icon from "./Icon.vue";
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
+import {useFileStore} from "../store";
+
+const fileStore = useFileStore();
+
+const pathList = ref(fileStore.currentPath.substring(1, fileStore.currentPath.length).split("/"));
+
+const pathBox = ref();
+const input = ref();
+const pathInput = ref();
+
+const isInput = ref(false);
 
 
-const path: string = "/AAAA/BBBBB/CCCCCCCCCC/DDDDDDDDDDDD"
-
-const pathList = path.split("/")
-
-const pathBox = ref()
-const input = ref()
-const pathInput = ref()
-
-const isInput = ref(false)
+watch(() => fileStore.currentPath, (path: string) => {
+  pathList.value = path.substring(1, path.length).split("/");
+});
 
 onMounted(() => {
-  const pathBoxEl = pathBox.value as Element
+  const pathBoxEl = pathBox.value as Element;
   pathBoxEl.addEventListener("wheel", (e) => {
-    e.preventDefault()
-    pathBoxEl.scrollLeft += e.deltaY
-  })
-})
+    e.preventDefault();
+    pathBoxEl.scrollLeft += (e.deltaY as number);
+  });
+});
 
 const changeInput = (e: Event) => {
-  e.stopPropagation()
-  isInput.value = true
-
-  const handle = (e: MouseEvent) => {
+  e.stopPropagation();
+  isInput.value = true;
+  const mouseHandle = (e: MouseEvent) => {
     if (!input.value.contains(e.target as Node)) {
-      isInput.value = false
-      document.removeEventListener("mousedown", handle)
+      isInput.value = false;
+      document.removeEventListener("mousedown", mouseHandle);
+      document.removeEventListener("keydown", keyHandle);
     }
   }
-  document.addEventListener("mousedown", handle)
+  const keyHandle = (e: KeyboardEvent) => {
+    if (e.code == "Enter") {
+      fileStore.setCurrentPath(pathInput.value.value);
+      isInput.value = false;
+      document.removeEventListener("mousedown", mouseHandle);
+      document.removeEventListener("keydown", keyHandle);
+    }
+  }
+
+  document.addEventListener("mousedown", mouseHandle);
+  document.addEventListener("keydown", keyHandle);
 }
 
+const changePath = (index: number) => {
+  if (index == -1) fileStore.setCurrentPath("/");
+  else fileStore.setCurrentPath(pathList.value.slice(0, index + 1).join("/"));
+}
 
 </script>
 
 <template>
   <sub-card class="path-card">
     <div ref="pathBox" class="path-box" :class="isInput? 'hidden':'flex'">
+      <div class="flex items-center gap-1">
+        <div class="path-item px-1" @click="changePath(-1)"><icon icon="icon-homefill" size="large" /></div>
+        <div class="separator"><icon icon="icon-unfold" class="-rotate-90" /></div>
+      </div>
       <div class="flex items-center gap-1" v-for="(pathItem, index) in pathList">
-        <div class="path-item"><span>{{ pathItem }}</span></div>
+        <div class="path-item px-2" @click="changePath(index)"><span>{{ pathItem }}</span></div>
         <div class="separator" v-if="index != pathList.length - 1"><icon icon="icon-unfold" class="-rotate-90" /></div>
       </div>
     </div>
     <div class="grow h-full min-w-14 cursor-text" :class="isInput? 'hidden':''" @click="changeInput">
     </div>
     <div ref="input" class="grow" :class="isInput? 'flex':'hidden'">
-      <input ref="pathInput" class="w-full h-10 px-3" :value="path">
+      <input ref="pathInput" class="w-full h-10 px-3" :value="fileStore.currentPath">
     </div>
-
   </sub-card>
 </template>
 
@@ -64,7 +86,7 @@ const changeInput = (e: Event) => {
   @apply px-2 shrink overflow-x-scroll
 }
 .path-item {
-  @apply inline-flex items-center h-7 px-2 hover:bg-blue-100 rounded cursor-pointer
+  @apply inline-flex items-center h-7 hover:bg-blue-100 rounded cursor-pointer
 }
 .separator {
   @apply w-4 h-7 inline-flex items-center justify-center
--- ui/src/store/index.ts
+++ ui/src/store/index.ts
@@ -3,18 +3,29 @@ import {FileInfo, FileTreeData, FolderData, FolderInfo} from "../class.ts";
 
 
 declare type FileState = {
-    currentFolder: string;
+    currentPath: string;
     folderData: Map<string, FolderData>;
 }
 
 export const useFileStore = defineStore('file', {
     state: (): FileState => ({
         // 当前文件路径
-        currentFolder: '' as string,
+        currentPath: '/' as string,
         // 文件夹数据  key为文件夹路径
         folderData: new Map() as Map<string, FolderData>,
     }),
     actions: {
+        // 设置当前路径
+        setCurrentPath(path: string) {
+            let tempPath = path
+            while (tempPath.indexOf("//") != -1) {
+                tempPath = tempPath.replace("//", "/")
+            }
+            if (tempPath.startsWith("/")) tempPath = tempPath.substring(1, tempPath.length)
+            if (tempPath.endsWith("/")) tempPath = tempPath.substring(0, tempPath.length - 1)
+            this.currentPath = "/" + tempPath
+        },
+
         // 保存转换文件夹数据到文件树
         saveAndConvertFolderData(data: FolderData): FileTreeData[] {
             const treeData: FileTreeData[] = [];
